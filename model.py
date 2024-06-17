db740f41c9f924004c92eb391f3ba4ff5fbdf125
Michael Rosol <michaelrosol26@gmail.com>
2024-06-17 15:23:55 -0400

    New Changes

--- model.py
+++ model.py
@@ -22,7 +22,7 @@ class MaskedAutoEncoder(nn.Module):
 
         # Encoder Blocks: 
         self.blocks = nn.ModuleList([
-            Block(emb_size, num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            Block(emb_size, num_heads, mlp_ratio, qkv_bias=True, norm_layer=norm_layer)
             for i in range(encoder_depth)])
         self.norm = norm_layer(emb_size)
 
@@ -33,7 +33,7 @@ class MaskedAutoEncoder(nn.Module):
         self.mask_token = nn.Parameter(torch.zeros(1,1, decoder_emb_size))
 
         self.decoder_blocks = nn.ModuleList([
-            Block(decoder_emb_size, decoder_num_heads, mlp_ratio, qkv_bias=True, qk_scale=None, norm_layer=norm_layer)
+            Block(decoder_emb_size, decoder_num_heads, mlp_ratio, qkv_bias=True, norm_layer=norm_layer)
             for i in range(decoder_depth)])
 
         self.decoder_pred = nn.Linear(decoder_emb_size, patch_size**2 * in_chans, bias=True)
@@ -45,22 +45,41 @@ class MaskedAutoEncoder(nn.Module):
             Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size)
         )
 
-        def forward(self, x, mask_ratio):
-            x = self.pos_embed
+        def random_masking(self, x, mask_ratio):
+            """
+            Perform per-sample random masking by per-sample shuffling.
+            Per-sample shuffling is done by argsort random noise.
+            x: [N, L, D], sequence
+            """
+            B, L, D = x.shape  # batch, length, dim
+            len_keep = int(L * (1 - mask_ratio))
+        
+            noise = torch.rand(B, L, device=x.device)  # noise in [0, 1]
+
+            # sort noise for each sample, then ids_shuffle to keep original index
+            ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
+            ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+            # keep the first subset
+            ids_keep = ids_shuffle[:, :len_keep]
+            x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+            # generate the binary mask: 0 is keep, 1 is remove
+            mask = torch.ones([B, L], device=x.device)
+            mask[:, :len_keep] = 0
+            # unshuffle to get the binary mask
+            mask = torch.gather(mask, dim=1, index=ids_restore)
+
+            return x_masked, mask, ids_restore
 
 
-# # Test the MaskedAutoEncoder class for patch embedding
+        def forward_encoder(self, x, mask_ratio):
+            x = self.pos_embed(x)
+
+        
+# Test the MaskedAutoEncoder class for patch embedding
 # if __name__ == "__main__":
-#     # Create a random tensor to represent a batch of images (batch_size, channels, height, width)
-#     batch_size = 1
-#     img_size = 224
-#     patch_size = 16
-#     in_chans = 3
-#     emb_size = 1024
-#     dummy_images = torch.randn(batch_size, in_chans, img_size, img_size)
-
-#     # Initialize and apply the encoder
-#     model = MaskedAutoEncoder(emb_size=emb_size, patch_size=patch_size, in_chans=in_chans, img_size=img_size)
-#     patch_embeddings = model(dummy_images)
-
-#     print("Patch Embeddings Shape:", patch_embeddings.shape)
+#     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#     x = torch.rand(1,3, 224, 224).to(device)
+#     model = MaskedAutoEncoder().to(device)
+#     print(model(x)[1].shape)
